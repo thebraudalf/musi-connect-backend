c01f8af3cd36c6036e1c68f371ebeafbbe76128a
Nehal Jain <niihaljain2005@gmail.com>
2026-05-06 13:09:45 +0530

    refactor database connection logic to use helper function for multiple databases

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/Music-Connect`);
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MongoDB connection FAILED", error);
-        process.exit(1);
-    }
-}
+dotenv.config();
 
-export default connectDB;   
+// Helper function to create a connection
+const makeNewConnection = (uri, dbName) => {
+    const db = mongoose.createConnection(`${uri}`, {
+        dbName: dbName,
+    });
+
+    db.on("connected", () => console.log(`MongoDB connected to ${dbName}`));
+    db.on("error", (err) => console.log(`MongoDB ${dbName} connection FAILED`, err));
+
+    return db;
+};
+
+
+export const userDb = makeNewConnection(process.env.MONGODB_URI, "User");
+export const chatMessageDb = makeNewConnection(process.env.MONGODB_URI, "Chat-Message");
+export const listeningHistoryDb = makeNewConnection(process.env.MONGODB_URI, "Listening-History");
