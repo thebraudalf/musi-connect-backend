bef2978b66de339a8d6642dd9d91a1932732b86a
Nehal Jain <niihaljain2005@gmail.com>
2026-05-06 13:59:18 +0530

    add comments to app server

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import dotenv from "dotenv";
-import connectDB from "./db/connection.js";
+import {
+  userDb,
+  chatMessageDb,
+  listeningHistoryDb,
+} from "./db/connection.js";
 import { httpServer } from "./app.js";
 
 dotenv.config();
 
 // connecting to MongoDB and setting up server
-connectDB()
-    .then(() => {
-        httpServer.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running at port: ${process.env.PORT}`);
-        })
-    })
-    .catch((error) => {
-        console.log("MongoDB connection failed! ", error);
-    });
+const startServer = async () => {
+  try {
+    // Wait for connections to establish
+    await Promise.all([
+      userDb.asPromise(),
+      chatMessageDb.asPromise(),
+      listeningHistoryDb.asPromise(),
+    ]);
 
-    
+    httpServer.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running. Multi-DB connections established.`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to databases", error);
+  }
+};
+
+startServer();
