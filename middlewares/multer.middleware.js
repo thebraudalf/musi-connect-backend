f7b1184c6727ef613bb7e73fd1a30a9545555f8c
Nehal Jain <niihaljain2005@gmail.com>
2026-05-06 13:16:51 +0530

    add comments to multer middleware logic

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,5 +1,11 @@
 import multer from "multer";
 
+/**
+ * @constant storage
+ * Configures the Multer Disk Storage engine.[cite: 10]
+ * - 'destination': Defines the local folder path ('./public/temp') where files are temporarily held.[cite: 10]
+ * - 'filename': Retains the original name of the uploaded file for easier identification.[cite: 10]
+ */
  const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./public/temp")
@@ -10,6 +16,11 @@ import multer from "multer";
 
  })
 
+/**
+ * @constant upload
+ * Initialized Multer instance using the disk storage configuration.[cite: 10]
+ * This instance is used as middleware in routes to handle multipart/form-data (file uploads).[cite: 10]
+ */ 
  export const upload = multer({
     storage: storage
  })
