6cb3c307eff083937f9f7ab42c896ccc6c2e7afa
Nehal Jain <niihaljain2005@gmail.com>
2026-05-06 13:20:56 +0530

    add comments to search  middleware logic

--- middlewares/search.middleware.js
+++ middlewares/search.middleware.js
@@ -2,36 +2,22 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
 import Joi from "joi";
 
-// middleware for search validation
+/**
+ * @middleware `searchValidation`
+ * Validates the integrity of search songs.
+ * Ensures 'searchQuery' is a string with at least 5 characters to provide enough context to get search results.
+ */
 const searchValidation = asyncHandler((req, res, next) => {
-    const schema = Joi.object({
-        searchQuery: Joi.string().min(5),
-    });
-    const { error } = schema.validate(req.body);
-    if (error) {
-        console.log("error", error);
-        return res.status(400)
-            .json(new ApiError(400, "Bad request: ", error))
-    }
+  const schema = Joi.object({
+    searchQuery: Joi.string().min(5),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) {
+    console.log("error", error);
+    return res.status(400).json(new ApiError(400, "Bad request: ", error));
+  }
 
-    next();
-})
+  next();
+});
 
-// middleware for songEvents validation
-const songEventsValidation = asyncHandler((req, res, next) => {
-    const schema = Joi.object({
-        userId: Joi.string().min(1).required(),
-        songId: Joi.string().min(1).required(),
-        eventType: Joi.string().min(1).required(),
-    });
-    const { error } = schema.validate(req.body);
-    if (error) {
-        console.log("error", error);
-        return res.status(400)
-            .json(new ApiError(400, "Bad request: ", error))
-    }
-
-    next();
-})
-
-export { searchValidation, songEventsValidation }
+export { searchValidation };
