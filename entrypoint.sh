0a8ddff6adcfb27f1f742c3e4b391faa79a4b486
Nehal Jain <niihaljain2005@gmail.com>
2026-05-06 14:01:15 +0530

    add changes to docker and entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Log in to Fluvio
-fluvio cloud login --email $FLUVIO_CLOUD_EMAIL --password $FLUVIO_CLOUD_PASSWORD
-
-# syncing configs
-fluvio cloud cluster sync
-
-
 # Start the application
 exec npm run server
