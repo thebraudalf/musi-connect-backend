6fc855180fb60bbb2c57e464f456817abcdc9c9c
Nehal Jain <niihaljain2005@gmail.com>
2026-05-06 13:58:31 +0530

    add comments to app routing

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ import { initializeSocketIO } from "./utils/socket/index.js";
 
 const app = express();
 
+/**
+ * @constant httpServer
+ * Creates an HTTP server instance using the Express 'app'. 
+ * This is necessary to integrate Socket.io with the same port as the web server.
+ */
 const httpServer = createServer(app);
 
+/**
+ * @constant io
+ * Initializes the Socket.io Server with specific configurations:
+ * - 'pingTimeout': Sets how long the server waits for a client response before closing the connection.
+ * - 'cors': Restricts socket access to the authorized 'CORS_ORIGIN' for security.
+ */
 const io = new Server(httpServer, {
     pingTimeout: 60000,
     cors: {
@@ -19,7 +30,7 @@ const io = new Server(httpServer, {
 
 app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global
 
-
+// Global Middlewares
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -30,19 +41,21 @@ app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// import routes
+/**
+ * @section Routes
+ * Defines the API versioning (v1) and maps specific URL paths to their respective routers:
+ * - '/api/v1/users': Handles registration, login, and profile management.
+ * - '/api/v1/messages': Manages AI-chat interactions and song recommendations.
+ * - '/api/v1/songs': Handles song fetching, searching, and metadata.
+ */
 import userRouter from "./routes/user.route.js";
 import messageRouter from "./routes/message.route.js";
 import songRouter from "./routes/song.route.js";
-import albumRouter from "./routes/album.route.js"
-import playlistRouter from "./routes/playlist.route.js"
 
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/songs", songRouter);
-app.use("/api/v1/albums", albumRouter);
-app.use("/api/v1/playlists", playlistRouter);
 
 initializeSocketIO(io); // initialize the socket io
 
